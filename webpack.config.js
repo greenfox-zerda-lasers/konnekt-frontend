5a0114020baca0774fa141b9a70fd7ee568be25d
h-attila <email.atinak@gmail.com>
2017-01-19 21:12:08 +0100

    login html new design

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,27 @@
 
 
 module.exports = {
-   entry:'./web/src/main.js',
+  entry: './web/src/main.js',
 
-   output: {
-      path:'./web/dist',
-      filename:'bundle.js'
-   },
+  output: {
+    path: './web/dist',
+    filename: 'bundle.js',
+  },
 
-   module: {
-      loaders: [
-         {
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader',
-            query: {
-               presets: ['es2015']
-            }
-         },
-         {
-            test: /\.scss$/,
-            loader: 'style-loader!css-loader!sass-loader'
-         }
-      ]
-   }
-}
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        },
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader',
+      },
+    ],
+  },
+};
