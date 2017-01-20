0ffeecdca5fe8609bd84a25d7a7fc6a77eb92b63
h-attila <email.atinak@gmail.com>
2017-01-20 12:04:39 +0100

    image deleted

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,31 @@
 
 
 module.exports = {
-   entry:'./web/src/main.js',
+  entry: './web/src/main.js',
 
-   output: {
-      path:'./web/dist',
-      filename:'bundle.js'
-   },
-
-   module: {
-      loaders: [
-         {
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader',
-            query: {
-               presets: ['es2015']
-            }
-         },
-         {
-            test: /\.scss$/,
-            loader: 'style-loader!css-loader!sass-loader'
-         }
-      ]
-   }
-}
+  output: {
+    path: './web/dist',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        },
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        path: './web/dist',
+        loader: 'image-webpack-loader',
+      },
+    ],
+  },
+};
