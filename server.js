5214fb7a22a653103040398071d0ad1bf3439022
h-attila <email.atinak@gmail.com>
2017-02-02 23:02:49 +0100

    some nightwork

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var server = require('express');
 var bodyParser = require('body-parser');
+var responseOK;
 
 
 // START SERVER
@@ -8,13 +9,15 @@ var app = server();
 var responseToken;
 var registerdUsers = ['Helga', 'Balazs', 'Attila'];
 
-app.use(server.static(__dirname + '/web'));
+app.use(server.static(`${__dirname}/web`));
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   if (registerdUsers.indexOf(req.body.email) >= 0 && req.body.password === req.body.email) {
-    responseToken = `Hello, ${req.body.email}`;
+    responseOk = true;
+    responseToken = 'Hi, I am a reponse token here!';
   } else {
-    responseToken = 'Hello, unknown';
+    responseOk = false;
+    responseToken = '';
   }
   res.setHeader('session_token', responseToken);
   next();
@@ -38,7 +41,20 @@ app.post('/register', function (req, res) {
 
 app.post('/login', function (req, res) {
   console.log('login on server');
-  res.send('success login');
+  if (responseOk) {
+    let response = { user_id: 0 }
+    res.status(201).send(JSON.stringify(response));
+  } else {
+    let response = {
+      errors: [
+        {
+          name: 'Unknown user error',
+          message: 'not user by this name',
+        },
+      ],
+    };
+    res.status(401).send(JSON.stringify(response));
+  }
 });
 
 // app.post('/login', function (req, res) {
