024df556790ce75342341f70320d1d5b9fafe5f2
h-attila <email.atinak@gmail.com>
2017-02-06 17:03:18 +0100

    working on contact list

--- web/src/app.js
+++ web/src/app.js
@@ -69,9 +69,14 @@ konnektApp.factory('HttpService', ['$http', function ($http) {
     return $http.post(`${appUrl}/register`, JSON.stringify(userData));
   }
 
+  function getContacts(userData) {
+    return $http.get(`${appUrl}/contacts`, JSON.stringify(userData));
+  }
+
   return {
     login: login,
     register: register,
+    getContacts: getContacts,
   };
 }]);
 
@@ -187,8 +192,29 @@ konnektApp.factory('UserService', ['HttpService', '$window', function (HttpServi
 }]);
 
 
-//CONTROLLERS
-konnektApp.controller('registrationController', ['$scope', '$http', function ($scope, $http) {
+konnektApp.factory('ContactService', ['UserService', 'HttpService', function (UserService, HttpService) {
+
+  let contactsData = {};
+
+  function getAllContacts() {
+    HttpService.getContacts().then(function (successResponse) {
+      contactsData = successResponse.data;
+      console.log(contactsData);
+    }, function (errorResponse) {
+      console.log(errorResponse.data);
+    });
+  }
+
+  return {
+    getAllContacts: getAllContacts,
+
+  };
+}]);
+
+
+// CONTROLLERS
+konnektApp.controller('registrationController', ['$scope', 'UserService', function ($scope, UserService) {
+>>>>>>> working on contact list
 
   $scope.header = 'regisztrálj.';
   $scope.welcome = 'üdv a Konnekt Kontaktkezelőben!';
@@ -229,7 +255,8 @@ konnektApp.controller('loginController', ['$scope', 'UserService', function ($sc
   };
 }]);
 
-konnektApp.controller('dashboardController', ['$scope', '$window', 'UserService', function ($scope, $window, UserService) {
+konnektApp.controller('dashboardController', ['$scope', '$window', 'UserService', 'ContactService', function ($scope, $window, UserService, ContactService) {
 
   $scope.header = UserService.getUserData().email;
+  $scope.contactsData = ContactService.getAllContacts();
 }]);
