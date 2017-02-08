3709df9e6102bc3c84032efe5a8b2fd7cf43bdb7
h-attila <email.atinak@gmail.com>
2017-02-09 00:43:21 +0100

    token try

--- web/src/app.js
+++ web/src/app.js
@@ -129,6 +129,8 @@ konnektApp.factory('UserService', ['HttpService', '$window', function (HttpServi
           console.log(successResponse);
           console.log('header');
           console.log(successResponse.headers());
+          console.log('getAllResponseHeaders()');
+          console.log(HttpService.getAllResponseHeaders());
           // console.log(successResponse.headers.session_token);
           // console.log(successResponse.headers['session_token']);
           newUserData.token = successResponse.headers().session_token;
