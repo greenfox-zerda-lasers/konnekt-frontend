73bb4ca6461c8093276582239df6906da67b72e0
h-attila <email.atinak@gmail.com>
2017-02-08 14:28:08 +0100

    url change

--- web/src/app.js
+++ web/src/app.js
@@ -3,10 +3,10 @@
 // *****************************************************************************
 //
 // for localhost testing
-const appUrl = 'http://localhost:3000';
+// const appUrl = 'http://localhost:3000';
 //
 // for lasers web
-// const appUrl = 'https://lasers-cornubite-konnekt.herokuapp.com';
+const appUrl = 'https://lasers-cornubite-konnekt.herokuapp.com';
 //
 // for raptors web
 // const appUrl = 'https://raptor-konnekt.herokuapp.com';
