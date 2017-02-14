56ffdddfb1c7162be5f39ec1272d7f741ec8a667
Helga <helga.varadi1@gmail.com>
2017-02-14 11:51:26 +0100

    createController is written, debugging is pending

--- web/src/app.js
+++ web/src/app.js
@@ -244,19 +244,27 @@ konnektApp.controller('dashboardController', ['$scope', '$window', 'UserService'
   $scope.header = UserService.getUserData().email;
 }]);
 
-konnektApp.controller('createController', ['$scope', 'UserService', function ($scope, UserService) {
+konnektApp.controller('createController', ['$scope', '$window', 'UserService', function ($scope, $window, UserService) {
 
   $scope.create_header = 'új kontakt';
   $scope.create_welcome = 'Adj hozzá egy hasznos ismerőst!';
   $scope.button = 'mehet';
 
-  // $scope.createName = createName;
-  // $scope.createDesc = createDesc;
-  //
-  // $scope.createContact = function () {
-  //   let newContactData = {};
-  //   newContactData.name = createName;
-  //   newContactData.desc = createDesc;
-  // };
+  var contactData = {
+    name: '',
+    description: ''
+  };
+
+  function saveContact(newContactData) {
+    contactData = Object.assign(contactData, newContactData);
+    console.log('saved contact data: ', contactData);
+  };
+
+  $scope.addContact = function () {
+    let newContactData = {};
+    newContactData.name = $scope.createName;
+    newContactData.desc = $scope.createDesc;
+    saveContact(newContactData);
+  };
 
 }]);
